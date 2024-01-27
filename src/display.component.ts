afa26f6223bba67b8682dd5e542658fbffe449e3
Maciek <maciej.piotrowski.external@atos.net>
2024-01-27 17:45:50 +0100

    display refactor

--- src/display.component.ts
+++ src/display.component.ts
@@ -1,30 +1,40 @@
 import {PokemonResponse} from "./models";
 const searchResults = document.getElementById('searchResults') as HTMLUListElement
+const error = document.getElementById('error') as HTMLDivElement
 
 export function displayResults(result: PokemonResponse) {
     if(searchResults === null) throw new Error('search-results element not found');
-    searchResults.innerHTML = '';
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `<h3>${result.name}</h3>`
-    searchResults.appendChild(listItem);
-    result.abilities.forEach(ability => {
-        const listItem = document.createElement('li');
-        listItem.innerHTML = `<p>${ability.ability.name}</p>`
-        searchResults.appendChild(listItem);
-    });
-}
-export  function displayError(response: Response, query: string){
-    if (response.status === 404) {
-        searchResults.innerHTML = '';
-        const listItem = document.createElement('li');
-        listItem.innerText = `Error! pokemon ${query} not found`;
-        searchResults.appendChild(listItem);
-    } else {
-        throw new Error(`Error ${response.status}: ${response.statusText}`);
-    }
+    clearResults();
+    clearError();
 
+    const pokemonName= document.createElement('h3');
+    pokemonName.classList.add('display-4');
+    pokemonName.innerHTML = `${result.name}`;
+    searchResults.appendChild(pokemonName);
+    const abilitiesHead= document.createElement('p');
+    abilitiesHead.innerHTML = `Pokemon abilities`;
+    searchResults.appendChild(abilitiesHead);
+    result.abilities.forEach(ability => {
+        const abilityParagraph= document.createElement('p');
+        abilityParagraph.innerHTML = `${ability.ability.name}`
+        searchResults.appendChild(abilityParagraph);
+    });
+    const separator= document.createElement('hr');
+    separator.classList.add('my-4');
+    searchResults.appendChild(separator);
+}
+export  function displayError(msg: string){
+        clearResults();
+        clearError();
+        const errorItem = document.createElement('p');
+        errorItem.classList.add('alert', 'alert-danger');
+        errorItem.innerText =msg
+        error.appendChild(errorItem);
 }
 
 export function clearResults() {
     searchResults.innerHTML = '';
+}
+export function clearError() {
+    error.innerHTML = '';
 }
