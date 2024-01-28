f77e9368371401471da7fbf7ecd6222f43323ab6
Maciek <maciej.piotrowski.external@atos.net>
2024-01-28 16:41:08 +0100

    service refactor

--- src/api.service.ts
+++ src/api.service.ts
@@ -1,27 +1,41 @@
 import {clearResults} from "./display.component";
 import {pokemonStore} from "./store";
 import {POKEMON_ERROR, POKEMON_UPDATE} from "./actions";
+import {PokemonResponse} from "./models";
 
-export async function sendRequest(query: string) {
+export class PokemonService {
+    private readonly baseURL = 'https://pokeapi.co/api/v2';
 
-    if (!query) {
-        clearResults()
-        return;
+    constructor() {
+
     }
-    if(query.length < 3) return;
-    query = query.toLowerCase();
-    try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${query}`);
-        if (!response.ok) {
-            if (response.status === 404) {
-                const error = `Error! pokemon ${query} not found`;
-                    pokemonStore.dispatch({ type:POKEMON_ERROR, payload: {error} });
+
+    getPokemonByName = async (query: string): Promise<void> => {
+        if (this.isQueryInvalid(query)) return;
+        query = query.toLowerCase();
+        try {
+            const response: Response = await fetch(`${this.baseURL}/pokemon/${query}`);
+            if (!response.ok) {
+                if (response.status === 404) {
+                    const error = `Error! pokemon ${query} not found`;
+                    pokemonStore.dispatch({type: POKEMON_ERROR, payload: {error}});
+                }
+                return;
             }
-            return;
+            const data = await response.json() as PokemonResponse;
+            pokemonStore.dispatch({type: POKEMON_UPDATE, payload: data});
+        } catch (errorRes) {
+            const error = `Error fetching data:${errorRes}`;
+            pokemonStore.dispatch({type: POKEMON_ERROR, payload: {error}});
         }
-        const data = await response.json();
-        pokemonStore.dispatch({ type:POKEMON_UPDATE, payload: data });
-    } catch (error) {
-        console.error('Error fetching data:', error);
+    }
+
+    private isQueryInvalid(query: string): boolean {
+        if (!query) {
+            clearResults();
+            return false
+        }
+        return query.length < 3;
+
     }
 }
